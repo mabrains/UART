fa3da5760c0284c2c11855e9ef04b173bcede378
Mariamzanaty <mariamzanaty839@yahoo.com>
2021-07-27 00:45:11 +0000

    Multiple_Baud_Generator_Logic

--- src/uart_baud_generator.v
+++ src/uart_baud_generator.v
@@ -1,27 +1,70 @@
 module uart_baud_generator(clk, tick);
 
+parameter clock_frequency = 9'd150_000_000,
+	  Baud_Rate_1 = 'd1200,
+	  Baud_Rate_2 = 'd2400,
+	  Baud_Rate_3 = 'd4800,
+	  Baud_Rate_4 = 'd9600,
+	  Baud_Rate_5 = 'd14400,
+	  Baud_Rate_6 = 'd38400,
+	  Baud_Rate_7 = 'd57600,
+	  Baud_Rate_8 = 'd115200;
+
 input clk;
 output[3:0] tick;
 reg tick;
-reg [9:0] acc= 10'b0;
-
+reg [16:0] acc= 17'b0;
+reg [16:0] Baud_Rate;
+reg [16:0] count = 17'b0;
 /*parameter  clock_frequency= 'd150_000_000,
            Baud_Rate= 'd9600,
            over_sampling_mode= 5'b10000,
            divisor= clock_frequency/(Baud_Rate* over_sampling_mode);
 */
 
+always @(posedge clk) begin
+	case (Baud_Rate)
+		Baud_Rate_1: begin
+			acc = 'd125008;
+		end
+		Baud_Rate_2: begin
+			acc = 'd62496;
+		end
+		Baud_Rate_3: begin
+			acc = 'd31248;
+		end
+		Baud_Rate_4: begin
+			acc = 'd15632;
+		end
+		Baud_Rate_5: begin
+			acc = 'd10416;
+		end
+		Baud_Rate_6: begin
+			acc = 'd3904;
+		end
+		Baud_Rate_7: begin
+			acc = 'd2608;
+		end
+		Baud_Rate_8: begin
+			acc = 'd1296;
+		end
+		default: begin
+			acc = 'd15632;
+		end
 
-always @ (posedge clk)
-begin
+	end
 
-if(acc ==10'b1111_010_001 )
+
+
+always @ (posedge clk) begin
+
+if(count == acc )
 begin
 tick <= tick+ 4'b0001;  //until it reaches 4'b1111 and overflows to reach 4'b0000 again
-acc[9:0] <= 10'b000_000_000;
+count <= 17'b000_000_000;
 end
 else
-acc = acc +9'b000_000_001;
+count = count +17'b000_000_001;
 
 end
 
