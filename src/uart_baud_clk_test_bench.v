766479df350dad82a3d31a08d273bd048340458d
Mariamzanaty <mariamzanaty839@yahoo.com>
2021-07-28 23:00:07 +0200

    uart_baud_generator

--- src/uart_baud_clk_test_bench.v
+++ src/uart_baud_clk_test_bench.v
@@ -1,16 +1,22 @@
-`timescale 1ns/1ns
+`timescale 1ns / 1ps
 module uart_baud_clk_tb();
 
-
 reg clk; 
+reg[16:0] Baud_Rate;
 wire[3:0] tick;
+
+reg[15:0] counter;
 
 parameter period= 6.66;
 
-
 //DUT_instantiation
-uart_baud_generator uart_tb ( .clk(clk) , .tick(tick) );
+uart_baud_generator uart_tb (
+       	.clk(clk), 
+	.tick(tick),
+	.Baud_Rate(Baud_Rate)
+);
 
+initial clk = 0;
 
 //clk_gen
 always 
@@ -19,15 +25,72 @@ begin
 #(period/2) clk = 0;
 end
 
+always @(tick) begin
+if(tick == 4'b1111)
+	counter <= counter +1;
+else
+	counter <= 16'b0;
+end
+
+always @(counter) begin
+	if (counter == 'd19200) begin
+		$display("everything is going well");
+	$finish;
+end
+end
 
 
-
-initial
-
+//Baud_Rate Determining for the transmitting bits
+initial 
 begin
-$time_format(-9,3,"ns");
-$display("        ", "     Time clk tick");
-$monitor("%t %t %b " , $realtime, clk, tick);
+#(2*period)
+Baud_Rate = 'd1200;
+repeat (150_000_000) @(posedge clk);
+    $display("Baud_Rate = 1200");
+   
+
+Baud_Rate = 'd2400;
+repeat (150_000_000) @(posedge clk);
+    $display("Baud_Rate = 2400");
+
+Baud_Rate = 'd4800;
+repeat (150_000_000) @(posedge clk);
+    $display("Baud_Rate = 4800");
+
+Baud_Rate = 'd9600;
+repeat (150_000_000) @(posedge clk);
+    $display("Baud_Rate = 9600");
+
+Baud_Rate = 'd14400;
+repeat (150_000_000) @(posedge clk);
+    $display("Baud_Rate = 144400");
+
+Baud_Rate = 'd38400;
+repeat (150_000_000) @(posedge clk);
+    $display("Baud_Rate = 38400");
+
+Baud_Rate = 'd57600;
+repeat (150_000_000) @(posedge clk);
+    $display("Baud_Rate = 57600");
+
+Baud_Rate = 'd115200;
+repeat (150_000_000) @(posedge clk);
+    $display("Baud_Rate = 115200");
+     $display("Nicely Done, Test Succeeded");
+    $finish;
+
+end
+
+initial 
+begin
+	repeat (12500_000_000) @(posedge clk);
+        $display("Something went wrong, Test Failed");
+$finish;
+end
+
+always @(Baud_Rate or tick) begin
+$timeformat(-9,1,"ns", 7);
+$display("time = %d, clock = %b,  Baud_Rate = %d, tick =  %b \n " , $time, clk, Baud_Rate, tick,);
 end
 
 endmodule
--- src/uart_baud_generator.v
+++ src/uart_baud_generator.v
@@ -1,74 +1,66 @@
-module uart_baud_generator(clk, tick);
+module uart_baud_generator(clk, rst, Baud_Rate, tick);
 
-parameter clock_frequency = 9'd150_000_000,
-	  Baud_Rate_1 = 'd1200,
-	  Baud_Rate_2 = 'd2400,
-	  Baud_Rate_3 = 'd4800,
-	  Baud_Rate_4 = 'd9600,
-	  Baud_Rate_5 = 'd14400,
-	  Baud_Rate_6 = 'd38400,
-	  Baud_Rate_7 = 'd57600,
-	  Baud_Rate_8 = 'd115200;
+parameter clock_frequency = 'd150_000_000;
 
-input clk;
-output[3:0] tick;
-reg tick;
-reg [16:0] acc= 17'b0;
-reg [16:0] Baud_Rate;
+input clk, rst;
+input [16:0] Baud_Rate;
+output[3:0] tick;   //for 16x higher than the baud rate for correct bit sampling
+
 reg [16:0] count = 17'b0;
-/*parameter  clock_frequency= 'd150_000_000,
-           Baud_Rate= 'd9600,
-           over_sampling_mode= 5'b10000,
-           divisor= clock_frequency/(Baud_Rate* over_sampling_mode);
+reg tick = 4'b0;
+reg [16:0] acc= 17'b0;       //accumulator * 16 = number of pulses per bit,  this value of number of pulses in 1 second is the baud rate required
+
+/*always @(posedge clk or negedge rst)
+	if(!rst)
+		tick <= 4'b0;
+	else
 */
-
-always @(posedge clk) begin
-	case (Baud_Rate)
-		Baud_Rate_1: begin
-			acc = 'd125008;
-		end
-		Baud_Rate_2: begin
-			acc = 'd62496;
-		end
-		Baud_Rate_3: begin
-			acc = 'd31248;
-		end
-		Baud_Rate_4: begin
-			acc = 'd15632;
-		end
-		Baud_Rate_5: begin
-			acc = 'd10416;
-		end
-		Baud_Rate_6: begin
-			acc = 'd3904;
-		end
-		Baud_Rate_7: begin
-			acc = 'd2608;
-		end
-		Baud_Rate_8: begin
-			acc = 'd1296;
-		end
-		default: begin
-			acc = 'd15632;
-		end
-
-	end
-
-
 
 always @ (posedge clk) begin
 
 if(count == acc )
 begin
 tick <= tick+ 4'b0001;  //until it reaches 4'b1111 and overflows to reach 4'b0000 again
-count <= 17'b000_000_000;
+count = 17'b000_000_000;
 end
 else
-count = count +17'b000_000_001;
+count <= count +17'b000_000_001;
 
 end
 
 
+always @(posedge clk) begin
+	case (Baud_Rate)
+		'd1200: begin
+			acc = 'd7813;
+		end
+		'd2400: begin
+			acc = 'd3906;
+		end
+		'd4800: begin
+			acc = 'd1953;
+		end
+		'd9600: begin
+			acc = 'd977;
+		end
+		'd14400: begin
+			acc = 'd10416;
+		end
+		'd38400: begin
+			acc = 'd244;
+		end
+		'd57600: begin
+			acc = 'd163;
+		end
+		'd115200: begin
+			acc = 'd81;
+		end
+		default: begin
+			acc = 'd977;
+		end
+
+	endcase
+end
 
 
 endmodule
